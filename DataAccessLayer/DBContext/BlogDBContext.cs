ab2cabfc9c8e57ffff04ae27025a4ba657a1cf0e
Ashwak Rayan Mohammad <ashwakrayan.m@motivitylabs.com>
2023-03-13 17:53:19 +0530

    Add project files.

---
+++ DataAccessLayer/DBContext/BlogDBContext.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DBContext
+{
+    public class BlogDBContext:DbContext
+    {
+        public BlogDBContext(DbContextOptions<BlogDBContext> options)
+         : base(options)
+        {
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
